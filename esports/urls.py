815ea8a1b0d731e332aaf4e563d36df337b8905a
GiomarMC <gmunozcu@unsa.edu.pe>
2025-08-15 18:31:33 -0500

    Changing URLs by routers

--- esports/urls.py
+++ esports/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 from .views import AdminLoginView
 
-urlpatterns = [
-    path('admin/login/', AdminLoginView.as_view(), name='admin-login'),
-]
+
+router = DefaultRouter()
+router.register(r'admin', AdminLoginView, basename='admin-login')
+
+urlpatterns = router.urls
