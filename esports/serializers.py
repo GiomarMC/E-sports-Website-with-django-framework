169ecc034fffdfbca63cf703ece05f5e14619036
GiomarMC <gmunozcu@unsa.edu.pe>
2025-08-20 19:23:50 -0500

    feat(serializers): add serializers for admin management
    - Added ChangePasswordSerializer for authenticated users to update their password
    - Added ResetPasswordSerializer for superadmins to reset other admins' passwords
    - Added AdminListSerializer to display admin and superadmin data with related games
    - Added AdminCreateSerializer to allow superadmins to create new admin or superadmin users

--- esports/serializers.py
+++ esports/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
 from rest_framework.exceptions import AuthenticationFailed, PermissionDenied
+from .models import CustomUser, AdminGame, Game
 
 
 class AdminLoginSerializer(serializers.Serializer):
@@ -28,3 +30,64 @@ class AdminLoginSerializer(serializers.Serializer):
 
         data['user'] = user
         return data
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    new_password = serializers.CharField(write_only=True)
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
+
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ['id', 'name']
+
+
+class AdminListSerializer(serializers.ModelSerializer):
+    games = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'role', 'games']
+
+    def get_games(self, obj):
+        if obj.role == 'superadmin':
+            return GameSerializer(Game.objects.all(), many=True).data
+
+        elif obj.role == 'admin':
+            qs = AdminGame.objects.filter(admin=obj)
+            return GameSerializer([ag.game for ag in qs], many=True).data
+        return []
+
+
+class AdminCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'role']
+
+    def validate_role(self, value):
+        if value not in ['admin', 'superadmin']:
+            raise serializers.ValidationError("Invalid role.")
+        return value
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            role=validated_data['role']
+        )
+        return user
