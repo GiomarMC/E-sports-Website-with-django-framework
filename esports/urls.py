4ea3de78906269a97c37831bcecd6d4719c562a0
GiomarMC <gmunozcu@unsa.edu.pe>
2025-08-24 20:39:02 -0500

    feat(routes): register GameViewSet in router for game management endpoints

--- esports/urls.py
+++ esports/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import AdminViewSet
+from .views import AdminViewSet, GameViewSet
 
 
 router = DefaultRouter()
 router.register(r'admin', AdminViewSet, basename='admin')
+router.register(r'games', GameViewSet, basename='games')
 
 urlpatterns = router.urls
